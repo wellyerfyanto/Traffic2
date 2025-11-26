22fd962d8c6b3a138ccc0fb5954f3a3bcf027277
wellyerfyanto <34988538+wellyerfyanto@users.noreply.github.com>
2025-11-26 23:29:05 +0700

    Update trafficGenerator.js

--- bot/trafficGenerator.js
+++ bot/trafficGenerator.js
@@ -1,5 +1,3 @@
-[file name]: trafficGenerator.js
-[file content begin]
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const UserAgents = require('user-agents');
@@ -799,4 +797,3 @@ class TrafficGenerator {
 }
 
 module.exports = TrafficGenerator;
-[file content end]
