3b38a64b0506faa02776b4b28962d1642d19dbe2
wellyerfyanto <34988538+wellyerfyanto@users.noreply.github.com>
2025-11-26 23:41:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const TrafficGenerator = require('./trafficGenerator.js');
+const TrafficGenerator = require('./bot/trafficGenerator.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
